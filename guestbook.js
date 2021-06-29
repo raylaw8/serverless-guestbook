78090a71d886d28ceaf649b26f2cfc0039db41a1
raylaw8 <52289430+raylaw8@users.noreply.github.com>
2021-06-29 23:58:18 +0800

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://c4fc00c5.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
